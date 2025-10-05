ee53d5bdb9f3e61c1758dc940d12461b2d09e1cf
Linda Schönfeldt <linda.schonfeldt@gmail.com>
2025-10-05 23:17:57 +0200

    feat: add About page component with basic structure and content

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+const About = () => {
+  return (
+    <Container>
+      <Title>About</Title>
+      <Text>
+        This is the about page. Here you can include information about yourself,
+        your background, skills, and experience.
+      </Text>
+      <Button>Learn More</Button>
+    </Container>
+  )
+}
+
+export default About
